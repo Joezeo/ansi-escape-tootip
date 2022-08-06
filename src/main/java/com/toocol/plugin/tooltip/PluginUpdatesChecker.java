61b4a05cb2f514a8788b007a93eaaf49d8f2d8f8
JoeZane <joezane.cn@gmail.com>
2022-08-06 22:17:05 +0800

    1. Update: anis escape tooltip progress

---
+++ src/main/java/com/toocol/plugin/tooltip/PluginUpdatesChecker.java
@@ -0,0 +1,27 @@
+package com.toocol.plugin.tooltip;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import com.toocol.plugin.tooltip.util.AnisEscapeSequenceTooltipNotification;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ：JoeZane (joezane.cn@gmail.com)
+ * @date: 2022/8/6 20:10
+ * @version: 0.0.1
+ */
+public class PluginUpdatesChecker implements StartupActivity.Background {
+    private static final Logger logger = LoggerFactory.getLogger(PluginUpdatesChecker.class);
+
+    @Override
+    public void runActivity(@NotNull Project project) {
+        logger.warn("Check plugin update project = {}", project);
+        checkOnProjectOpened(project);
+    }
+
+    public void checkOnProjectOpened(@NotNull Project project) {
+        AnisEscapeSequenceTooltipNotification.showUpdate(project);
+    }
+}
---
+++ src/main/java/com/toocol/plugin/tooltip/search/AnisEscapeSearchEngine.java
@@ -0,0 +1,20 @@
+package com.toocol.plugin.tooltip.search;
+
+import com.toocol.plugin.tooltip.escape.AnisEscapeSequence;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author ：JoeZane (joezane.cn@gmail.com)
+ * @date: 2022/8/6 21:14
+ * @version: 0.0.1
+ */
+public class AnisEscapeSearchEngine {
+    private static final Pattern[] ESCAPE_PATTERNS = new Pattern[] {
+
+    };
+
+    public AnisEscapeSequence getEscapeSequence(String text) {
+        return null;
+    }
+}
